1061664a71710be59389744f615ebf60354f3590
Sergio Cortés <sergiocortmon@gmail.com>
2026-01-19 10:38:49 +0100

    feat: move currencies to static class

---
+++ jobs/Backend/Task/Data/SupportedCurrencies.cs
@@ -0,0 +1,27 @@
+﻿using ExchangeRateUpdater.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRateUpdater.Data
+{
+    public static class SupportedCurrencies
+    {
+        public static readonly IEnumerable<Currency> All = new[]
+        {
+            new Currency("USD"),
+            new Currency("EUR"),
+            new Currency("CZK"),
+            new Currency("JPY"),
+            new Currency("KES"),
+            new Currency("RUB"),
+            new Currency("THB"),
+            new Currency("TRY"),
+            new Currency("XYZ")
+        };
+
+        public static readonly Currency DefaultBaseCurrency = new Currency("CZK");
+    }
+}
--- jobs/Backend/Task/Program.cs
+++ jobs/Backend/Task/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ExchangeRateUpdater.Application.Providers;
+using ExchangeRateUpdater.Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -6,19 +8,6 @@ namespace ExchangeRateUpdater
 {
     public static class Program
     {
-        private static IEnumerable<Currency> currencies = new[]
-        {
-            new Currency("USD"),
-            new Currency("EUR"),
-            new Currency("CZK"),
-            new Currency("JPY"),
-            new Currency("KES"),
-            new Currency("RUB"),
-            new Currency("THB"),
-            new Currency("TRY"),
-            new Currency("XYZ")
-        };
-
         public static void Main(string[] args)
         {
             try
