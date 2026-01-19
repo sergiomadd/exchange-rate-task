63644dcc3a8878ccda4b56f13bf231ef7b0a740c
Sergio Cortés <sergiocortmon@gmail.com>
2026-01-19 10:52:21 +0100

    feat: add config and needed packages

---
+++ jobs/Backend/Task/Infrastructure/Options/CnbApiOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRateUpdater.Infrastructure.Options
+{
+    public class CnbApiOptions
+    {
+        public const string SectionName = "CnbApi";
+        public string BaseUrl { get; set; }
+        public string DailyRatesPath { get; set; }
+    }
+}
--- jobs/Backend/Task/Program.cs
+++ jobs/Backend/Task/Program.cs
@@ -1,8 +1,17 @@
 ﻿using ExchangeRateUpdater.Application.Providers;
 using ExchangeRateUpdater.Core.Entities;
+using ExchangeRateUpdater.Data;
+using ExchangeRateUpdater.Infrastructure.Options;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace ExchangeRateUpdater
 {
@@ -10,6 +19,23 @@ namespace ExchangeRateUpdater
     {
         public static void Main(string[] args)
         {
+            using var host = Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((context, config) =>
+            {
+                config
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false)
+                .AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            })
+            .ConfigureLogging((context, logging) =>
+            {
+                logging.ClearProviders();
+                logging.AddConfiguration(context.Configuration.GetSection("Logging"));
+                logging.AddConsole();
+            })
+            .Build();
+
             try
             {
                 var provider = new ExchangeRateProvider();
