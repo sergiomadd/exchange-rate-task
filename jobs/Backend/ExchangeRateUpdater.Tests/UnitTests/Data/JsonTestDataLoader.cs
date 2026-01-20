fba7d9bc35ae631f192ccf58e4f845ff61064f7f
Sergio Cortés <sergiocortmon@gmail.com>
2026-01-20 12:47:46 +0100

    test: add test project

---
+++ jobs/Backend/ExchangeRateUpdater.Tests/UnitTests/Data/JsonTestDataLoader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRateUpdater.Tests.UnitTests.Data
+{
+    public static class JsonTestDataLoader
+    {
+        public static string Load(string relativePath)
+        {
+            var fullPath = Path.Combine(AppContext.BaseDirectory, "UnitTests", "Data", relativePath);
+            return File.ReadAllText(fullPath);
+        }
+    }
+}
