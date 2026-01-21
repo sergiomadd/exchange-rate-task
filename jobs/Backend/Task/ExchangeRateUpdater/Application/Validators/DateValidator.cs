4bc67449b36ddf0ee3ba472d29f6623315dbf608
Sergio Cortés <sergiocortmon@gmail.com>
2026-01-21 13:25:42 +0100

    feat: add DateValidator

---
+++ jobs/Backend/Task/ExchangeRateUpdater/Application/Validators/DateValidator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRateUpdater.Application.Validators
+{
+    public static class DateValidator
+    {
+        public static bool ValidateDate(string input)
+        {
+            DateTime date;
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine("No date entered.");
+                return false;
+            }
+
+            if (!DateTime.TryParseExact(input, "yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out date))
+            {
+                Console.WriteLine("Invalid date format. Please enter date as yyyy-MM-dd.");
+                return false;
+            }
+
+            if (date > DateTime.Today)
+            {
+                Console.WriteLine("Date cannot be in the future. Please enter a valid past or current date.");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
